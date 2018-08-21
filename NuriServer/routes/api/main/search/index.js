a1441d331017d97e0e08b7343519da055f1c4bf8
kooseolwon <seolwon6289@naver.com>
2018-08-22 01:16:44 +0900

    add seach directory

--- NuriServer/routes/api/main/search/index.js
+++ NuriServer/routes/api/main/search/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const searchRouter = require('./search/index');
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
     res.render('index', { title: 'Express' });
   });
-router.use('/search',searchRouter);
   
 
 module.exports = router;
