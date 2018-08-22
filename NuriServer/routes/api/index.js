0ff0acfef19d24615155986dd0cfd0d847ea4cd1
kooseolwon <seolwon6289@naver.com>
2018-08-23 00:54:01 +0900

    add user router

--- NuriServer/routes/api/index.js
+++ NuriServer/routes/api/index.js
@@ -5,11 +5,12 @@ const courseRouter = require('./course/index');
 const infoRouter = require('./info/index');
 const mainRouter = require('./main/index');
 const mypageRouter = require('./mypage/index');
+const userRouter = require('./user/index');
 
 router.use('/course',courseRouter);
 router.use('/info',infoRouter);
 router.use('/main',mainRouter);
 router.use('/mypage',mypageRouter);
-
+router.use('/user',userRouter);
 
 module.exports = router;
