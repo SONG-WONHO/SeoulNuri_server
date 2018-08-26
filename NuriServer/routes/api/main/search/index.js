7ed3f6301465782ff095a4f348a491fef9a9ce26
KIMSJ <leesd556@gmail.com>
2018-08-26 20:53:10 +0900

    랜덤관광지

--- NuriServer/routes/api/main/search/index.js
+++ NuriServer/routes/api/main/search/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const handi_type_reco = require('../../../../module/tour/filterTour')
+const handi_type_reco = require('../../../../module/tour/randomTour')
 
 router.get('/', async (req, res, next) => {
 	let test
 
-	test = await handi_type_reco.get_filter_tour([0,2])
+	test = await handi_type_reco.get_random_tour()
 })
 // /* GET home page. */
 // router.get('/', (req, res, next) => {
