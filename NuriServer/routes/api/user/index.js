22b11108ca7bb4a2136046ef784e35c0a437551e
kooseolwon <seolwon6289@naver.com>
2018-08-27 03:38:48 +0900

    회원가입시 장애유형 선택 라우터 추가

--- NuriServer/routes/api/user/index.js
+++ NuriServer/routes/api/user/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const mypage = require('../../../module/mypage/mypage.js');
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-    res.render('index', { title: 'Express' });
+router.post('/',async(req,res,next)=>{
+    let handiList = req.body.handi_type;
+    if(handiList.length===0){
+        next("400");
+        return;
+    }
+    let result = await mypage.post_handiType(handiList,req.user_idx);
+    if(result === false){
+        next("500");
+        return;
+    }
+    res.r();
 });
 
 module.exports = router;
