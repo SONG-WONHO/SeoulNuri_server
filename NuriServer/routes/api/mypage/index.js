54df3b20fc4cdba78f02847964acba5e12cd19cf
kooseolwon <seolwon6289@naver.com>
2018-08-27 03:29:21 +0900

    마이페이지 장애유형 변경

--- NuriServer/routes/api/mypage/index.js
+++ NuriServer/routes/api/mypage/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const mypage = require('../../../module/mypage/mypage.js');
 /* GET home page. */
 router.get('/', (req, res, next) => {
     res.render('index', { title: 'Express' });
@@ -11,6 +11,20 @@ router.get('/', (req, res, next) => {
 const bookmarkRouter = require('./bookmark');
 router.use('/bookmark', bookmarkRouter);
 
+//
+router.post('/',async(req,res,next)=>{
+    let handiList = req.body.handi_type;
+    if(handiList.length===0){
+        next("400");
+        return;
+    }
+    let result = await mypage.post_handiType(handiList,req.user_idx);
+    if(result === false){
+        next("500");
+        return;
+    }
+    res.r();
+});
 
 
 module.exports = router;
