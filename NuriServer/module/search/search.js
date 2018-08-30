f02c38e0801277bb9a14630966f722d303ab5b90
kooseolwon <seolwon6289@naver.com>
2018-08-31 01:28:38 +0900

    검색 기능

---
+++ NuriServer/module/search/search.js
@@ -0,0 +1,33 @@
+const db = require('../db');
+
+module.exports = {
+    get_search : async (word)=>{
+        let selectQuery = `SELECT tour_idx, tour_name
+        FROM tour`;
+        
+        let selectResult = await db.queryParamNone(selectQuery);
+        if(!selectResult){
+            return false;
+        }
+
+        selectResult = selectResult.filter((value)=>{
+
+            if(word === "" ){
+                
+                return true;
+            }
+            else{
+                if(value["tour_name"].indexOf(word) !== -1){
+                    
+                    return true;
+                }
+                
+                
+            }
+        });
+
+        return selectResult;
+        
+    }
+
+};
--- NuriServer/routes/api/main/search/index.js
+++ NuriServer/routes/api/main/search/index.js
@@ -2,12 +2,30 @@ const express = require('express');
 const router = express.Router();
 
 const a = require('../../../../module/tour/filterTour')
+const search = require('../../../../module/search/search.js');
+
 
 router.get('/', async(req,res,next)=>{
 
  let b = await a.get_filter_tour([1,2,3,4],[0,2,10,20,22, 31])	
 
 })
+
+//메인에서 검색창 키워드 넣었을 때
+router.get('/keyword', async(req,res,next)=>{
+    let word = req.query.word || "";
+    
+    let result = await search.get_search(word);
+    if(!result){
+        next("500");
+        return;
+    }
+    res.r(result);
+    
+
+
+
+});
 
 // /* GET home page. */
 // router.get('/', (req, res, next) => {
