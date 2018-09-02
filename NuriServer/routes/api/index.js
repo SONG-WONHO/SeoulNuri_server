d9f743c802bdb00ffc5b5828c39482f1b0075399
kooseolwon <seolwon6289@naver.com>
2018-09-02 17:30:47 +0900

    플래너 라우터 추가

--- NuriServer/routes/api/index.js
+++ NuriServer/routes/api/index.js
@@ -6,11 +6,14 @@ const infoRouter = require('./info/index');
 const mainRouter = require('./main/index');
 const mypageRouter = require('./mypage/index');
 const userRouter = require('./user/index');
+const plannerRouter = require('./planner/index');
+
 
 router.use('/course',courseRouter);
 router.use('/info',infoRouter);
 router.use('/main',mainRouter);
 router.use('/mypage',mypageRouter);
 router.use('/user',userRouter);
+router.use('/planner', plannerRouter);
 
 module.exports = router;
