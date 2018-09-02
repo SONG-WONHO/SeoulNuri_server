e662ddd5d149f418b485f9e647f2a09af50cfc7f
kooseolwon <seolwon6289@naver.com>
2018-09-02 23:23:30 +0900

    유저부분 장애유형추가에  trycatch문 추가

--- NuriServer/routes/api/user/index.js
+++ NuriServer/routes/api/user/index.js
@@ -6,16 +6,26 @@ const signupRouter = require('./signup')
 router.use('/signup', signupRouter)
 
 router.post('/',async(req,res,next)=>{
-    let handiList = req.body.handi_type;
+    let result;
+
+    try {
+        let handiList = req.body.handi_type;
     if(handiList.length===0){
         next("400");
         return;
     }
-    let result = await mypage.post_handiType(handiList,req.user_idx);
+    result = await mypage.post_handiType(handiList,req.user_idx);
     if(result === false){
         next("500");
         return;
     }
+        
+    } catch (err) {
+        next(err);
+        return;
+        
+    }
+    
     res.r();
 });
 
