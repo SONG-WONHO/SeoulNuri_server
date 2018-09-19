5ceaaf17af69edcf48d3716ab18bbf613717161a
kooseolwon <seolwon6289@naver.com>
2018-09-19 21:48:04 +0900

    add js

--- nuriserver/routes/api/planner/add/add.js
+++ nuriserver/routes/api/planner/add/add.js
@@ -7,25 +7,23 @@ router.post('/', async(req,res,next) =>{
 	let plan_date = req.body.plan_date
 	let tour_idx = req.body.tour_idx
 	let data = {}
-	console.log(user_idx);
-	console.log(tour_idx);
-	console.log(plan_date);
 
-	try {
-		
-		if(!user_idx || !plan_date || !tour_idx){
-			next("400");
-			return
-		}
-		
-		let result = await planner.add_plan(user_idx, plan_date,tour_idx)
-		data.plan_idx = result
+	 try {
 
-	} catch (err) {
-		next(err)
-		return
-	}
-	res.r(data)
+	 	if(!user_idx || !plan_date || !plan_tours){
+	 		next("400")
+	 		return
+	 	}
+	 	let result = await planner.add_plan(user_idx, plan_date,tour_idx)
+
+	 	data.plan_idx = result
+
+	 } catch (err) {
+	 	next(err)
+	 	return
+	 }
+
+	 res.r(data)
 })
 
 module.exports = router
