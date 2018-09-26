c29df321cf6e20d153bc109ed4b0697bfe473b66
kooseolwon <seolwon6289@naver.com>
2018-09-26 15:53:05 +0900

    북마크 에러 처리

--- NuriServer/module/bookmark/bookmark.js
+++ NuriServer/module/bookmark/bookmark.js
@@ -28,15 +28,26 @@ module.exports = {
         
         let selectQuery = `
         SELECT *
-        FROM ${dbTable}
+        FROM user
         WHERE user_idx = ? 
         `;
         let selectResult = await db.queryParamArr(selectQuery,[user_idx]);
+        
+        console.log(selectResult[0]);
         if(selectResult.length >= 1){
+            let bookmarkQuery = `SELECT * FROM ${dbTable} WHERE ${bookmarkIdx}=? AND user_idx = ?`
+            let bookmakrResult = await db.queryParamArr(bookmarkQuery,[idx,user_idx]);
+            if(bookmakrResult[0]>=1){
+                
+                return false; 
+            }
             let insertQuery = `INSERT INTO ${dbTable} (${typeIdx}, user_idx) VALUES (?,?)`;
-
-            return await db.queryParamArr(insertQuery,[idx,user_idx]);
+            let result = await db.queryParamArr(insertQuery,[idx,user_idx])
+            console.log(result);
+            return result;
             
+        }else{
+            return false;
         }
         
     }
--- NuriServer/routes/api/course/bookmark.js
+++ NuriServer/routes/api/course/bookmark.js
@@ -6,12 +6,15 @@ const bookmark = require('../../../module/bookmark/bookmark.js');
 router.post('/', async (req,res,next)=>{
     try {
         let courseIdx = req.body.course_idx;
+        console.log(courseIdx)
+        console.log(req.user.user_idx)
         if(!courseIdx){
             next("400");
             return;
         }
         let result = await bookmark.post_bookmark("course",courseIdx,req.user.user_idx);
-        if(!result){
+        console.log(result);
+        if(!result || result === false){
             next("500");
             return;
         }
